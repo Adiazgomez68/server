e1405d0acc02e44232884c28f34cd5fb18ca9fe2
Administrador <administrador@iMac-de-Administrador.local>
2022-12-16 09:53:35 -0500

    Update file server.js

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const app = express()
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello Everybody, i'm here programming in this place!!!")
+    res.send("Hello Everybody, i'm here programming in this place!!!");
+}) 
+
+app.get("/message", (req, res) => {
+    res.send("This a message sent for first users");
 }) 
 
 app.listen(port, () => {
-    console.log("Server on port 3000")
+    console.log("Server on port 3000");
 })
